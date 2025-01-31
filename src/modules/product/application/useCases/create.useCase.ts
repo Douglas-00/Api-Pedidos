19206f7d6142b2ff4c60cfcb8e9d2ce6e47df602
DG-Tech00 <douglasalvessouza0@gmail.com>
2025-01-31 08:37:04 -0300

    Fix/ajustes

--- src/modules/product/application/useCases/create.useCase.ts
+++ src/modules/product/application/useCases/create.useCase.ts
@@ -3,19 +3,28 @@ import { ProductRepository } from '../domain/repositories/product.repository';
 import { ProductMapper } from '../mappers/product.mapper';
 import { CreateProductRequestDto } from '../../infra/dto/create/request.dto';
 import { CreateProductResponseDto } from '../../infra/dto/create/response.dto';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class CreateProductUseCase {
   constructor(
     @Inject('ProductPrismaRepository')
     private readonly productRepository: ProductRepository,
+    private readonly prisma: PrismaService,
   ) {}
 
   async execute(
     payload: CreateProductRequestDto,
   ): Promise<CreateProductResponseDto> {
     const product = ProductMapper.toDomain(payload);
-    const newProduct = await this.productRepository.create(product);
-    return { id: newProduct.id, message: 'Product created successfully' };
+
+    return await this.prisma.$transaction(async () => {
+      const newProduct = await this.productRepository.create(product);
+
+      return {
+        id: newProduct.id,
+        message: 'Product created successfully',
+      };
+    });
   }
 }
--- src/modules/product/application/useCases/update.useCase.ts
+++ src/modules/product/application/useCases/update.useCase.ts
@@ -4,6 +4,7 @@ import { AppLogger } from 'src/modules/logger/logger.service';
 import { ProductMapper } from '../mappers/product.mapper';
 import { UpdateProductRequestDto } from '../../infra/dto/update/request.dto';
 import { UpdateProductResponseDto } from '../../infra/dto/update/response.dto';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class UpdateProductUseCase {
@@ -11,24 +12,29 @@ export class UpdateProductUseCase {
     @Inject('ProductPrismaRepository')
     private readonly productRepository: ProductRepository,
     private readonly logger: AppLogger,
+    private readonly prisma: PrismaService,
   ) {}
 
   async execute(
     productId: number,
     updateProductDto: UpdateProductRequestDto,
   ): Promise<UpdateProductResponseDto> {
-    const verifyProduct = await this.productRepository.findById(productId);
+    return await this.prisma.$transaction(async () => {
+      const verifyProduct = await this.productRepository.findById(productId);
 
-    if (!verifyProduct) {
-      this.logger.warn('Product not found');
-      throw new BadRequestException('Product not found');
-    }
-    const product = await ProductMapper.toDomainUpdate(updateProductDto);
+      if (!verifyProduct) {
+        this.logger.warn('Product not found');
+        throw new BadRequestException('Product not found');
+      }
 
-    const updatedProduct = await this.productRepository.update(
-      productId,
-      product,
-    );
-    return ProductMapper.toUpdateResponseDto(updatedProduct);
+      const product = await ProductMapper.toDomainUpdate(updateProductDto);
+
+      const updatedProduct = await this.productRepository.update(
+        productId,
+        product,
+      );
+
+      return ProductMapper.toUpdateResponseDto(updatedProduct);
+    });
   }
 }
